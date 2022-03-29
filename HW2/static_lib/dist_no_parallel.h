f427c7f265cd8e1dfd28a2b1afa0a399c3e848e9
Fedor Safonov <safonovfedya696@gmail.com>
2022-03-29 22:45:08 +0300

    header fixed

--- HW2/static_lib/dist_no_parallel.h
+++ HW2/static_lib/dist_no_parallel.h
@@ -22,8 +22,6 @@ typedef struct Point2 {
 
 #endif
 
-static void fill_bits(int32_t* a);  // заполнить число случайными битами
-int32_t* create_mas(int sz);  // создаем массив с точками
 static Point2 get_point(int32_t a);  // прочитать из int32 4 числа int8
 static float dist(Point2 p);  // дистанция между (x1, y1) и (x2, y2)
 double count_sum_dist(int32_t* mas, int sz);  // посчитать суммарный путь по точкам в массиве
